72345cbf09316f41e7077d76422524e4fcb7fa9b
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-06-13 07:04:08 +0000

    fav button color changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
 
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/style.css" type="text/css" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,9 +1,9 @@
 @if (Auth::user()->is_favoriting($micropost->id))
     {!! Form::open(['route' => ['user.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-        {!! Form::submit('Unfavorite', ['class' => "btn btn-danger active btn-xs"]) !!}
+        {!! Form::submit('Favorited', ['class' => "btn btn-warning active btn-xs"]) !!}
     {!! Form::close() !!}
 @else
     {!! Form::open(['route' => ['user.favorite', $micropost->id]]) !!}
-        {!! Form::submit('Favorite', ['class' => "btn btn-warning btn-xs"]) !!}
+        {!! Form::submit('Favorite', ['class' => "btn  btn-xs"]) !!}
     {!! Form::close() !!}
 @endif
